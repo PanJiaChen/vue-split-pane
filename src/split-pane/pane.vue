38257cfd09b2a911dcbd5390d8b9f3f5e2523ae9
Anatoly Kolesov <kolesoffac@gmail.com>
2017-12-04 11:05:40 +0300

    indent with 2

--- src/split-pane/pane.vue
+++ src/split-pane/pane.vue
@@ -1,47 +1,47 @@
 <template>
-	<div :class="classes">
-		<slot></slot>
-	</div>
+  <div :class="classes">
+      <slot></slot>
+  </div>
 </template>
 
 <script>
-	export default {
-	  name: 'Pane',
-	  data() {
-	    const classes = ['Pane', this.$parent.split, 'className'];
-	    return {
-	      classes: classes.join(' '),
-	      percent: 50
-	    }
-	  }
-	}
+  export default {
+    name: 'Pane',
+    data() {
+      const classes = ['Pane', this.$parent.split, 'className'];
+      return {
+        classes: classes.join(' '),
+        percent: 50
+      }
+    }
+  }
 </script>
 
 <style scoped>
 .splitter-pane.vertical.splitter-paneL {
-    position: absolute;
-    left: 0px;
-    height: 100%;
-    padding-right: 3px;
+  position: absolute;
+  left: 0px;
+  height: 100%;
+  padding-right: 3px;
 }
 
 .splitter-pane.vertical.splitter-paneR {
-    position: absolute;
-    right: 0px;
-    height: 100%;
-    padding-left: 3px;
+  position: absolute;
+  right: 0px;
+  height: 100%;
+  padding-left: 3px;
 }
 
 .splitter-pane.horizontal.splitter-paneL {
-    position: absolute;
-    top: 0px;
-    width: 100%;
+  position: absolute;
+  top: 0px;
+  width: 100%;
 }
 
 .splitter-pane.horizontal.splitter-paneR {
-    position: absolute;
-    bottom: 0px;
-    width: 100%;
-    padding-top: 3px;
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  padding-top: 3px;
 }
 </style>
--- src/split-pane/resizer.vue
+++ src/split-pane/resizer.vue
@@ -1,87 +1,87 @@
 <template>
-	<div :class="classes"></div>
+    <div :class="classes"></div>
 </template>
 
 <script>
   export default {
-	props: {
-	  split: {
-		validator(value) {
-		  return ['vertical', 'horizontal'].indexOf(value) >= 0
-		},
-		required: true
-	  },
-	  className: String
-	},
-	data() {
-		return {}
-	},
-	computed: {
-		classes: function() {
-			const classes = ['Resizer', this.split, this.className];
+    props: {
+      split: {
+        validator(value) {
+          return ['vertical', 'horizontal'].indexOf(value) >= 0
+        },
+        required: true
+      },
+      className: String
+    },
+    data() {
+        return {}
+    },
+    computed: {
+      classes: function() {
+        const classes = ['Resizer', this.split, this.className];
 
-			return classes.join(' ');
-		}
-	}
+        return classes.join(' ');
+      }
+    }
   }
 </script>
 
 <style scoped>
 .Resizer {
-	-moz-box-sizing: border-box;
-	-webkit-box-sizing: border-box;
-	box-sizing: border-box;
-	position: absolute;
-	-moz-background-clip: padding;
-	-webkit-background-clip: padding;
-	background-clip: padding-box;
-	z-index: 1000;
-	background: black
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  position: absolute;
+  -moz-background-clip: padding;
+  -webkit-background-clip: padding;
+  background-clip: padding-box;
+  z-index: 1000;
+  background: black
 }
 .Resizer:hover {
-	background: grey
+  background: grey
 }
 
 .Resizer:after {
-	content: "";
-	display: block;
-	position: absolute;
-	background: #fff;
+  content: "";
+  display: block;
+  position: absolute;
+  background: #fff;
 }
 
 .Resizer:hover:after {
-	background: darkgrey
+  background: darkgrey
 }
 
 .Resizer.horizontal:after {
-	top: 25%;
-	left: 50%;
-	width: 24px;
-	height: 2px;
+  top: 25%;
+  left: 50%;
+  width: 24px;
+  height: 2px;
 }
 
 .Resizer.vertical:after {
-	top: 50%;
-	left: 25%;
-	width: 2px;
-	height: 24px;
+  top: 50%;
+  left: 25%;
+  width: 2px;
+  height: 24px;
 }
 
 .Resizer.horizontal {
-	margin: -5px 0;
-	cursor: row-resize;
-	width: 100%;
-	height: 6px;
-	border: 1px #afafaf solid;
-	margin-top: 0;
+  margin: -5px 0;
+  cursor: row-resize;
+  width: 100%;
+  height: 6px;
+  border: 1px #afafaf solid;
+  margin-top: 0;
 }
 
 .Resizer.vertical {
-	height: 100%;
-	margin-left: -5px;
-	cursor: col-resize;
-	width: 6px;
-	border: 1px #bdbdbd solid;
-	margin-left: -3px;
+  height: 100%;
+  margin-left: -5px;
+  cursor: col-resize;
+  width: 6px;
+  border: 1px #bdbdbd solid;
+  margin-left: -3px;
 }
 </style>
