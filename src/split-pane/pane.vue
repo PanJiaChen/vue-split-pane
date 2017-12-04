0573f6d1216d860c708889b4def6a084e755143d
Anatoly Kolesov <kolesoffac@gmail.com>
2017-12-04 10:14:42 +0300

    update style

--- src/split-pane/pane.vue
+++ src/split-pane/pane.vue
@@ -22,12 +22,14 @@
     position: absolute;
     left: 0px;
     height: 100%;
+    padding-right: 3px;
 }
 
 .splitter-pane.vertical.splitter-paneR {
     position: absolute;
     right: 0px;
     height: 100%;
+    padding-left: 3px;
 }
 
 .splitter-pane.horizontal.splitter-paneL {
@@ -40,5 +42,6 @@
     position: absolute;
     bottom: 0px;
     width: 100%;
+    padding-top: 3px;
 }
 </style>
