432f4d705f22464a886440c1b7e015bdb49fe77d
Anatoly Kolesov <kolesoffac@gmail.com>
2017-12-06 11:43:52 +0300

    refine style (#3)
    * add webpack watch
    
    * update style
    
    * build
    
    * indent with 2
    
    * update style

--- src/split-pane/resizer.vue
+++ src/split-pane/resizer.vue
@@ -1,5 +1,5 @@
 <template>
-	<div :class="classes"></div>
+    <div :class="classes"></div>
 </template>
 
 <script>
@@ -10,46 +10,78 @@
           return ['vertical', 'horizontal'].indexOf(value) >= 0
         },
         required: true
-      }
+      },
+      className: String
     },
     data() {
-      const classes = ['Resizer', this.split, 'className'];
-      return {
-        classes: classes.join(' ')
+        return {}
+    },
+    computed: {
+      classes: function() {
+        const classes = ['Resizer', this.split, this.className];
+
+        return classes.join(' ');
       }
     }
   }
 </script>
 
-<style scoped>.Resizer {
-	-moz-box-sizing: border-box;
-	-webkit-box-sizing: border-box;
-	box-sizing: border-box;
-	background: #000;
-	position: absolute;
-	opacity: .2;
-	z-index: 1;
-	-moz-background-clip: padding;
-	-webkit-background-clip: padding;
-	background-clip: padding-box;
+<style scoped>
+.Resizer {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  position: absolute;
+  -moz-background-clip: padding;
+  -webkit-background-clip: padding;
+  background-clip: padding-box;
+  z-index: 1000;
+  background: #b9b9b9;
+}
+.Resizer:hover {
+  background: #dadce0;
+}
+
+.Resizer:after {
+  content: "";
+  display: block;
+  position: absolute;
+  background: #fff;
+}
+
+.Resizer:hover:after {
+  background: darkgrey
+}
+
+.Resizer.horizontal:after {
+  top: 25%;
+  left: 50%;
+  width: 24px;
+  height: 2px;
+}
+
+.Resizer.vertical:after {
+  top: 50%;
+  left: 25%;
+  width: 2px;
+  height: 24px;
 }
 
 .Resizer.horizontal {
-	height: 11px;
-	margin: -5px 0;
-	border-top: 5px solid rgba(255, 255, 255, 0);
-	border-bottom: 5px solid rgba(255, 255, 255, 0);
-	cursor: row-resize;
-	width: 100%;
+  margin: -5px 0;
+  cursor: row-resize;
+  width: 100%;
+  height: 6px;
+  border: 1px #bdbdbd78 solid;
+  margin-top: 0;
 }
 
 .Resizer.vertical {
-	width: 11px;
-	height: 100%;
-	margin-left: -5px;
-	border-left: 5px solid rgba(255, 255, 255, 0);
-	border-right: 5px solid rgba(255, 255, 255, 0);
-	cursor: col-resize;
+  height: 100%;
+  margin-left: -5px;
+  cursor: col-resize;
+  width: 6px;
+  border: 1px #bdbdbd78 solid;
+  margin-left: -3px;
 }
-
 </style>
